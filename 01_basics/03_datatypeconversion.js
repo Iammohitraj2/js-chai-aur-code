e256b6a28c63999ecb9e1cf8bb4c74cd6152fea6
Iammohitraj2 <iammohitraj2@gmail.com>
2026-03-28 11:44:25 +0000

    conversion of data types

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,10 @@
+let score="33"
+console.log(typeof(score))
+let number= Number(score);
+console.log(score)
+console.log(typeof(number))
+
+let a= "33abc" 
+let convert= Number(a)
+console.log(typeof(convert))
+console.log(convert)
