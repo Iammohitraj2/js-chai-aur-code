2d4f3e1b5e6da65e16f28350ea4364e958f0258a
Iammohitraj2 <iammohitraj2@gmail.com>
2026-03-28 11:45:17 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId= 144553
+let accountEmail="iammohit@.com"
+var accountPass= "12345"
+accountCity="patna"
+
+//accountId=2 not allowed
+
+accountEmail="hello@.com"
+accountPass="111111"
+accountCity="delhi"
+console.table([accountId,accountEmail,accountPass,accountCity])
